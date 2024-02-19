52ae53d4388637c51eee86236ae796cf98559fc0
Clara Pecci <cp661@cam.ac.uk>
2024-02-19 13:00:41 +0000

    Include UMAP in alluvial comparison

--- alluvial.R
+++ alluvial.R
@@ -9,12 +9,15 @@ library(aricode)
 
 
 #### LOAD FILE USED IN CLUSTERING!!
+
 #Data used
-data_used<-read_csv("data/ASD_ADHD_global_5-10.csv")
+data_used<-read_csv("data/ASD_global_features.csv")
+
 #Directory where clustering assignments is saved
-results_dir <- "results/ASD_ADHD_5-10/"
+results_dir <- "results/ASD_global/"
+
 #Specify directory to save new plots
-saving_dir <- file.path(results_dir, 'new results')
+saving_dir <-file.path(results_dir, 'test')
 
 #Create directory to save results
 if (!file.exists(saving_dir)){ 
@@ -72,14 +75,14 @@ ggplot(combined_data, aes(y = stat(count), !!!axis_list)) +
        scale_x_discrete(limits = unlist(cluster_columns), expand = c(.05, .05))
 ggsave(filename)
 
-###### CHECK FOR TSNE
+###### CHECK FOR TSNE + umap
 #Check if directory contains tsne file 
 if (file.exists(file.path(results_dir, 'tsne_medoids'))){
   
   #Open tsne clustering with no control
   tsne_clustering <- read_csv(file.path(results_dir, 'tsne_medoids', 'control_FALSE', 'tsne_medoids_clustering.csv'))
   
-  #Check IDs are the samw across HYDRA and tSNE
+  #Check IDs are the same across HYDRA and tSNE
   stopifnot(tsne_clustering$ID == combined_data$ID)
   
   #Find number of clusters 
@@ -99,7 +102,45 @@ if (file.exists(file.path(results_dir, 'tsne_medoids'))){
     compare_plot_dir <- file.path(saving_dir, 'comparison_plot.png')
     
     #Calculate mutual information
-    NMI_clustering <-NMI(combined_data[[cluster_number_tsne]], combined_data$cluster_tsne)
+    NMI_clustering_tsne <-NMI(combined_data[[cluster_number_tsne]], combined_data$cluster_tsne)
+   
+    
+  #Check for umap clustering  
+  if (file.exists(file.path(results_dir, 'umap_medoids', 'control_FALSE'))){
+    
+    #Open tsne clustering with no control
+    umap_clustering <- read_csv(file.path(results_dir, 'umap_medoids', 'control_FALSE', 'umap_medoids_clustering.csv'))
+    
+    #Check IDs are the same across HYDRA and tSNE
+    stopifnot(umap_clustering$ID == combined_data$ID)
+    
+    #Find number of clusters 
+    cluster_number_umap <- nlevels(factor(umap_clustering$cluster_tsne))
+    
+    #Find equivalent number of clusters in HYDRA
+    cluster_number_umap <- paste0('cluster_', cluster_number_umap)
+    
+    #If number of clusters match
+    if (cluster_number_umap %in% colnames(combined_data)){
+      
+      #Concatenate tsne-medoids data with HYDRA
+      combined_data <- combined_data %>%
+        mutate(cluster_umap = umap_clustering$cluster_tsne)
+      
+      #Calculate mutual information
+      NMI_clustering_umap <-NMI(combined_data[[cluster_number_tsne]], combined_data$cluster_umap)
+      
+      ggplot(combined_data,
+             aes(y = stat(count), axis1 = .data[[cluster_number_tsne]], axis2 = cluster_tsne, axis3 = cluster_umap)) +
+        geom_alluvium(width = 1/12) +
+        geom_stratum(width = 1/12, fill = "black", color = "grey") +
+        scale_x_discrete(limits = c("HYDRA", "TSNE", "UMAP"), expand = c(.05, .05)) +
+        ggtitle(paste('NMI ', NMI_clustering_tsne, NMI_clustering_umap))
+      
+      ggsave(compare_plot_dir)
+    
+  }
+    }else{
     
     #Alluvial plot across techniques
     ggplot(combined_data,
@@ -107,12 +148,11 @@ if (file.exists(file.path(results_dir, 'tsne_medoids'))){
       geom_alluvium(width = 1/12) +
       geom_stratum(width = 1/12, fill = "black", color = "grey") +
       scale_x_discrete(limits = c("HYDRA", "TSNE"), expand = c(.05, .05)) +
-      ggtitle('NMI ', NMI_clustering)
+      ggtitle('NMI ', NMI_clustering_tsne)
     
     ggsave(compare_plot_dir)
     
-    
-    
+  } 
   }
   
   
