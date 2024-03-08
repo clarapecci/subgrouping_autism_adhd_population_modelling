0f0ceb69ce724b43fd6ceae276b3f0511c2df30d
Clara Pecci <cp661@cam.ac.uk>
2024-03-08 12:19:20 +0000

    In alluvial plot, include optimal clustering for HYDRA instead of matching to other techniques

--- alluvial.R
+++ alluvial.R
@@ -5,19 +5,17 @@ library(ggalluvial)
 library(ggrepel)
 library(aricode)
 # TO DO IN SCRIPT:
-#   - add sustain
-
 
 #### LOAD FILE USED IN CLUSTERING!!
 
 #Data used
-data_used<-read_csv("data/ASD_global_features.csv")
+data_used<-read_csv("data/ASD_ADHD_global.csv")
 
 #Directory where clustering assignments is saved
-results_dir <- "results/ASD_global/"
+results_dir <- "results/ASD_ADHD_global/"
 
 #Specify directory to save new plots
-saving_dir <-file.path(results_dir, 'test')
+saving_dir <-file.path(results_dir, 'comparisons')
 
 #Create directory to save results
 if (!file.exists(saving_dir)){ 
@@ -43,7 +41,10 @@ used_ID <- data_used$ID
 #Extract correct clinical features
 clinical_features <- clinical_data %>% 
   filter(ID %in% used_ID) %>%
-  select (-c(participant, site, dx.original, sex, ID))
+  select (-c(site, dx.original, sex, ID))
+
+#Extract list of participants for sustain
+participants_used <- clinical_features$participant
 
 #Extract non clinical features not used in clustering
 non_clinical_features <- all_features_data %>% 
@@ -55,93 +56,119 @@ for (i in 1:(number_clusters - 1)) {
   data_used[, cluster_columns[i]] <- clustered_data$CIDX[, i + 1]
 }
 
+
 #Combine with features
 combined_data <- data_used %>%
   cbind(clinical_features)%>%
   cbind(non_clinical_features)%>%
   filter(dx.original !='CN')
   
-##############ALLUVIAL PLOT ###########
 
+
+########### find NMI between clusters in same technique ########### 
+saving_df <- read_csv('results/NMI_in_technique.csv')
+#Create list of clusters used
+cluster_list <- seq(from = 2, to =dim(clustered_data$CIDX)[2] )
+
+#Create all combinations between clusters
+cluster_combs <- combn(cluster_list, 2)
+
+#Empty array to store values 
+loop_NMI <- numeric(length = dim(cluster_combs)[2])
+
+#Iterate over all cluster combinations
+for (i in 1:dim(cluster_combs)[2]){
+  
+  first_cluster <- paste0('cluster_', cluster_combs[1, i])
+  second_cluster <- paste0('cluster_', cluster_combs[2, i])
+  
+  #Calculate NMI between cluster pairs
+  loop_NMI[i] <- NMI(combined_data[[first_cluster]], combined_data[[second_cluster]])
+}
+
+print(paste('mean', mean(loop_NMI)))
+print(paste('sd', sd(loop_NMI)))
+
+#Create new row to add to data frame
+new_row <- tibble('File' =results_dir,
+                      'All_NMI' = list(array(loop_NMI)), 
+                      'Mean' = mean(loop_NMI),
+                      'Std' = sd(loop_NMI))
+
+#Save new data frame
+updated_df <- rbind(saving_df, new_row)
+write_csv(updated_df, 'results/NMI_in_technique.csv')
+
+
+############## ALLUVIAL PLOT ###########
 filename <- file.path(saving_dir, 'HYDRA_clusters.png')
 
 #Create list of axis based on cluster columns
 axis_list <- map(setNames(cluster_columns, paste0("axis", seq_along(cluster_columns))), ~ sym(.x))
 
 # Create the ggplot with axis_list
-ggplot(combined_data, aes(y = stat(count), !!!axis_list)) +
-      geom_alluvium(width = 1/12) +
-       geom_stratum(width = 1/12, fill = "black", color = "grey") +
-       scale_x_discrete(limits = unlist(cluster_columns), expand = c(.05, .05))
-ggsave(filename)
+ ggplot(combined_data, aes(y = stat(count),!!!axis_list)) +
+       geom_alluvium(width = 1/12) +
+        geom_stratum(width = 1/12, fill = "black", color = "grey", spacing = 1000) +
+   theme_bw()+
+        scale_x_discrete(limits = unlist(cluster_columns), expand = c(.05, .05))
+ ggsave(filename)
 
-###### CHECK FOR TSNE + umap
-#Check if directory contains tsne file 
+
+
+
+###### CHECK FOR TSNE + umap 
+#Check if directory contains tsne file
+#Find highest ARI cluster in HYDRA to compare to others
+highest_ARI_cluster <-  paste0('cluster_', which.max(clustered_data$ARI) ) 
+
+ 
 if (file.exists(file.path(results_dir, 'tsne_medoids'))){
-  
+
   #Open tsne clustering with no control
   tsne_clustering <- read_csv(file.path(results_dir, 'tsne_medoids', 'control_FALSE', 'tsne_medoids_clustering.csv'))
-  
+
   #Check IDs are the same across HYDRA and tSNE
   stopifnot(tsne_clustering$ID == combined_data$ID)
-  
-  #Find number of clusters 
-  cluster_number_tsne <- nlevels(factor(tsne_clustering$cluster_tsne))
-  
-  #Find equivalent number of clusters in HYDRA
-  cluster_number_tsne <- paste0('cluster_', cluster_number_tsne)
-  
-  #If number of clusters match
-  if (cluster_number_tsne %in% colnames(combined_data)){
-    
-    #Concatenate tsne-medoids data with HYDRA
-    combined_data <- combined_data %>%
-      mutate(cluster_tsne = tsne_clustering$cluster_tsne)
-    
-    #Create directory to save new plot
-    compare_plot_dir <- file.path(saving_dir, 'comparison_plot.png')
-    
-    #Calculate mutual information
-    NMI_clustering_tsne <-NMI(combined_data[[cluster_number_tsne]], combined_data$cluster_tsne)
-   
-    
-  #Check for umap clustering  
+
+  #Concatenate tsne-medoids data with HYDRA
+  combined_data <- combined_data %>%
+    mutate(cluster_tsne = tsne_clustering$cluster_tsne)
+
+  #Create directory to save new plot
+  compare_plot_dir <- file.path(saving_dir, 'comparison_plot.png')
+
+  #Calculate mutual information
+  NMI_clustering_tsne <-round(NMI(combined_data[[highest_ARI_cluster]], combined_data$cluster_tsne), 4)
+
+
+  #Check for umap clustering
   if (file.exists(file.path(results_dir, 'umap_medoids', 'control_FALSE'))){
-    
+
     #Open tsne clustering with no control
     umap_clustering <- read_csv(file.path(results_dir, 'umap_medoids', 'control_FALSE', 'umap_medoids_clustering.csv'))
-    
+
     #Check IDs are the same across HYDRA and tSNE
     stopifnot(umap_clustering$ID == combined_data$ID)
-    
-    #Find number of clusters 
-    cluster_number_umap <- nlevels(factor(umap_clustering$cluster_tsne))
-    
-    #Find equivalent number of clusters in HYDRA
-    cluster_number_umap <- paste0('cluster_', cluster_number_umap)
-    
-    #If number of clusters match
-    if (cluster_number_umap %in% colnames(combined_data)){
-      
-      #Concatenate tsne-medoids data with HYDRA
-      combined_data <- combined_data %>%
-        mutate(cluster_umap = umap_clustering$cluster_tsne)
-      
-      #Calculate mutual information
-      NMI_clustering_umap <-NMI(combined_data[[cluster_number_tsne]], combined_data$cluster_umap)
-      
-      ggplot(combined_data,
-             aes(y = stat(count), axis1 = .data[[cluster_number_tsne]], axis2 = cluster_tsne, axis3 = cluster_umap)) +
-        geom_alluvium(width = 1/12) +
-        geom_stratum(width = 1/12, fill = "black", color = "grey") +
-        scale_x_discrete(limits = c("HYDRA", "TSNE", "UMAP"), expand = c(.05, .05)) +
-        ggtitle(paste('NMI ', NMI_clustering_tsne, NMI_clustering_umap))
-      
-      ggsave(compare_plot_dir)
-    
-  }
+
+    #Concatenate tsne-medoids data with HYDRA
+    combined_data <- combined_data %>%
+      mutate(cluster_umap = umap_clustering$cluster_umap)
+
+    #Calculate mutual information
+    NMI_clustering_umap <-round(NMI(combined_data[[highest_ARI_cluster]], combined_data$cluster_umap), 4)
+    NMI_tsne_umap <- round(NMI(combined_data$cluster_tsne, combined_data$cluster_umap), 4)
+    ggplot(combined_data,
+           aes(y = stat(count), axis1 = .data[[cluster_number_tsne]], axis2 = cluster_tsne, axis3 = cluster_umap)) +
+      geom_alluvium(width = 1/12) +
+      geom_stratum(width = 1/12, fill = "black", color = "grey") +
+      scale_x_discrete(limits = c("HYDRA", "TSNE", "UMAP"), expand = c(.05, .05)) +
+      ggtitle(paste('NMI ', NMI_clustering_tsne, NMI_clustering_umap, NMI_tsne_umap))
+
+    ggsave(compare_plot_dir)
+
     }else{
-    
+
     #Alluvial plot across techniques
     ggplot(combined_data,
            aes(y = stat(count), axis1 = .data[[cluster_number_tsne]], axis2 = cluster_tsne)) +
@@ -149,11 +176,49 @@ if (file.exists(file.path(results_dir, 'tsne_medoids'))){
       geom_stratum(width = 1/12, fill = "black", color = "grey") +
       scale_x_discrete(limits = c("HYDRA", "TSNE"), expand = c(.05, .05)) +
       ggtitle('NMI ', NMI_clustering_tsne)
-    
+
     ggsave(compare_plot_dir)
-    
-  } 
+
   }
-  
-  
+
 }
+
+
+#Check number of significant features shared between clusters 
+hydra_dir <- list.files(file.path(results_dir, cluster_number_tsne, 'sig'))
+tsne_dir <- list.files(file.path(results_dir, 'tsne_medoids', 'control_FALSE', 'cluster', 'sig'))
+umap_dir <- list.files(file.path(results_dir, 'umap_medoids', 'control_FALSE', 'cluster', 'sig'))
+
+print(paste('HYDRA and tSNE: ', length(intersect(hydra_dir, tsne_dir))))
+print(paste('HYDRA and UMAP: ', length(intersect(hydra_dir, umap_dir))))
+print(paste('UMAP and tsne: ', length(intersect(umap_dir, tsne_dir))))
+
+
+
+# if (file.exists(file.path(results_dir, 'ADHD_sustain_subtypes_combat_3subgroups.csv'))){
+#   
+#   #Open tsne clustering with no control
+#   sustain_clustering <- read_csv(file.path(results_dir, 'ADHD_sustain_subtypes_combat_3subgroups.csv'))
+#   
+#   sustain_clustering <- sustain_clustering%>%
+#     filter(participant %in% participants_used)%>%
+#     filter(Diagnosis!='CN')
+#   
+#   #Concatenate tsne-medoids data with HYDRA
+#   combined_data <- combined_data %>%
+#     mutate(cluster_sustain = sustain_clustering$ml_subtype)
+#   
+#   #Calculate mutual information
+#   NMI_clustering_sustain <-NMI(combined_data[[cluster_number_tsne]], combined_data$cluster_sustain)
+#   ggplot(combined_data,
+#          aes(y = stat(count), axis1 = cluster_sustain, axis2 = .data[[cluster_number_tsne]], axis3 = cluster_tsne, axis4 = cluster_umap) )+
+#     geom_alluvium(width = 1/12) +
+#     geom_stratum(width = 1/12, fill = "black", color = "grey") +
+#     scale_x_discrete(limits = c("SUSTAIN", "HYDRA", "TSNE", "UMAP"), expand = c(.05, .05)) +
+#     ggtitle(paste('NMI ', NMI_clustering_tsne, NMI_clustering_umap, NMI_tsne_umap, NMI_clustering_sustain))
+#   
+#   #Create directory to save new plot
+#   compare_plot_dir_2 <- file.path(saving_dir, 'comparison_plot_sustain.png')
+#   ggsave(compare_plot_dir_2)
+#   
+# }
